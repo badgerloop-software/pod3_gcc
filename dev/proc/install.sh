417f0c6257bd0b636237ddb7173e53c86abdc705
Cooper Green <csgreen3@wisc.edu>
2017-10-30 15:36:04 -0500

    Command Line build
    Added a Cube MX project
    Successfully compile and flash from dev
    
    Signed-off-by: Cooper Green <csgreen3@wisc.edu>

---
+++ dev/proc/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ST-Link V2
+if [ -d "/media/$USERNAME/NODE_L432KC" ]; then
+	cp e2c.bin /media/$USERNAME/NODE_L432KC
+	echo "Copying badgerloop.bin to /media/$USERNAME/NODE_L432KC"
+# J-Link
+else
+	JLinkExe -device STML432KC -if SWD -speed 4000 -autoconnect 1 -CommanderScript ./proc/flash.jlink
+fi
+
