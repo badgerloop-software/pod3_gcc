0c1eb53e581e1b30cc184e03a99908bfa8b948fe
vkottler <vkottler@wisc.edu>
2017-11-22 01:49:57 -0600

    specify C++ standard to use

--- cc.mk
+++ cc.mk
@@ -11,7 +11,7 @@ FPU = -mfpu=fpv4-sp-d16 -mfloat-abi=softfp
 # -nostdlib -lnosys
 CFLAGS = $(FPU) $(ARCH_FLAGS) $(DEFINES) $(CPU_DEFINES) $(INCLUDES)
 CFLAGS += -Wall -ffunction-sections -fdata-sections -fno-builtin -Os
-CXXFLAGS=$(CFLAGS)
+CXXFLAGS=$(CFLAGS) -std=gnu++11
 
 # Linker Settings
 LFLAGS = --specs=nosys.specs -Wl,--gc-sections -Wl,-Map=$(PROJECT).map
