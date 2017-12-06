787d1049025b6f4a7e13c44b0a1cc25c93ad6820
vkottler <vkottler@wisc.edu>
2017-12-06 01:47:09 -0600

    cosmetic changes to configure.sh

--- configure.sh
+++ configure.sh
@@ -9,8 +9,7 @@ read -t 10 -p "Which device family? " DEV_FAM
 echo "Processors: " && ls proc/$DEV_FAM 2>/dev/null | sed 's/drivers//'
 echo ""
 if [ $? -eq 2 ]; then
-	echo "'$DEV_FAM' not found."
-	exit 1
+	echo "'$DEV_FAM' not found." && exit 1
 fi
 
 read -t 10 -p "Which processor? " PROC_CHOICE
@@ -18,20 +17,24 @@ read -t 10 -p "Which processor? " PROC_CHOICE
 # Verify processor dir exists
 ls proc/$DEV_FAM/$PROC_CHOICE >/dev/null 2>/dev/null
 if [ $? -eq 2 ]; then
-	echo "'$PROC_CHOICE' not found."
-	exit 1
+	echo "'$PROC_CHOICE' not found." && exit 1
 fi
 
 # Verify drivers dir exists
 ls proc/$DEV_FAM/drivers >/dev/null 2>/dev/null
 if [ $? -eq 2 ]; then
-	echo "'$DEV_FAM/drivers' not found."
-	exit 1
+	echo "'$DEV_FAM/drivers' not found." && exit 1
 fi
 
+# Remove old links
 rm -f cfg
 rm -f src/drivers
+
+# Make new links
 ln -s proc/$DEV_FAM/$PROC_CHOICE cfg
 ln -s ../proc/$DEV_FAM/drivers src/drivers
+
+# Clean outputs
 make clean
+
 echo "Project configured for $DEV_FAM$PROC_CHOICE."
